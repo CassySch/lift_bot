e0f302e963286992dba525f88c8ce71b4f5c0ae4
CassySch <94879749+CassySch@users.noreply.github.com>
2023-12-05 11:25:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ import config
 import machine
 import network
 import socket
+
+# motor1A = machine.Pin(18, machine.Pin.OUT)
+# motor1B = machine.Pin(25, machine.Pin.OUT)
+# motor1C = machine.Pin(26, machine.Pin.OUT)
+# stby = machine.Pin(27, machine.Pin.OUT)
+# pwma = machine.Pin(33, machine.Pin.OUT)
+lift_up = machine.Pin(15,machine.Pin.OUT)
+lift_down = machine.Pin(13,machine.Pin.OUT)
+# piezo = machine.PWM(machine.Pin(19))
+
+while 1:
+    lift_up.value(1)
+    lift_down.value(0)
