f6270e801773d6b468ea4e161a2058df06a421d4
CassySch <94879749+CassySch@users.noreply.github.com>
2023-12-13 17:45:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-import config
-import machine
-import network
-import socket
+from motor_control import Motors
 
-# motor1A = machine.Pin(18, machine.Pin.OUT)
-# motor1B = machine.Pin(25, machine.Pin.OUT)
-# motor1C = machine.Pin(26, machine.Pin.OUT)
-# stby = machine.Pin(27, machine.Pin.OUT)
-# pwma = machine.Pin(33, machine.Pin.OUT)
-lift_up = machine.Pin(15,machine.Pin.OUT)
-lift_down = machine.Pin(13,machine.Pin.OUT)
-# piezo = machine.PWM(machine.Pin(19))
+lift_bot = Motors(300, 500)
+while True:
+    lift_bot.connect()
+    lift_bot.button_handler()
 
-while 1:
-    lift_up.value(1)
-    lift_down.value(0)
