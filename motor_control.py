1fb6902ba114c5641d8477826c04cedf25af0b7c
CassySch <94879749+CassySch@users.noreply.github.com>
2023-12-13 22:15:35 -0500

    Update motor_control.py

--- motor_control.py
+++ motor_control.py
@@ -166,6 +166,7 @@ class Motors:
         pins.AIN1.value(1)
         pins.AIN2.value(0)
         pins.PWMA.duty(500)
+        pins.PWMB.duty(0)
 
     def go_backward(self):
         print("Backward")
@@ -192,6 +193,7 @@ class Motors:
         pins.BIN1.value(0)
         pins.BIN2.value(1)
         pins.PWMB.duty(500)
+        pins.PWMA.duty(0)
 
     def stop(self):
         print("Stopped")
