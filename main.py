5618a800541ef19a67bdd853990aa996469ad264
CassySch <94879749+CassySch@users.noreply.github.com>
2023-12-18 17:02:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import pins
 lift_bot = Motors(500, 500)
 lift_bot.connect()
 
-if not (pins.UP.value() == 0 and pins.DOWN.value() == 0):
+if not (pins.TOP.value() == 0 and pins.BOTTOM.value() == 0):
     lift_bot.lift_down()
     lift_bot.current_state = 9
-if pins.UP.value() == 0:
+if pins.TOP.value() == 0:
     lift_bot.lift_state = "OPEN"
-if pins.DOWN.value() == 0:
+if pins.BOTTOM.value() == 0:
     lift_bot.lift_state = "CLOSED"
 else:
     lift_bot.lift_state = "PARTIAL"
