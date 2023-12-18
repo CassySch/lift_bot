afe51b178d98780cba6a0b770493e346fd08cbbb
CassySch <94879749+CassySch@users.noreply.github.com>
2023-12-18 16:26:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ lift_bot.connect()
 if not (pins.UP.value() == 0 and pins.DOWN.value() == 0):
     lift_bot.lift_down()
     lift_bot.current_state = 9
-            
+if pins.UP.value() == 0:
+    lift_bot.lift_state = "OPEN"
+if pins.DOWN.value() == 0:
+    lift_bot.lift_state = "CLOSED"
+else:
+    lift_bot.lift_state = "PARTIAL"
 while True:
     lift_bot.button_handler()
     lift_bot.motor_control()
