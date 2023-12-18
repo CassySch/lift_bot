d0dfa06cd426582eb63b233d28e6bf9d70db8c60
CassySch <94879749+CassySch@users.noreply.github.com>
2023-12-18 16:22:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from motor_control import Motors
+import pins
 
-lift_bot = Motors(1000, 500)
+lift_bot = Motors(500, 500)
 lift_bot.connect()
+
+if not (pins.UP.value() == 0 and pins.DOWN.value() == 0):
+    lift_bot.lift_down()
+    lift_bot.current_state = 9
+            
 while True:
     lift_bot.button_handler()
     lift_bot.motor_control()
