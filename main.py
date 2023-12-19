7b2d74f09426ce3e3dd241ee0e0d2832b6d5cafb
CassySch <94879749+CassySch@users.noreply.github.com>
2023-12-19 10:29:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import pins
 lift_bot = Motors(500, 500)
 lift_bot.connect()
 
-if not (pins.TOP.value() == 0 and pins.BOTTOM.value() == 0):
+if not pins.TOP.value() == 0 or not pins.BOTTOM.value() == 0:
     lift_bot.lift_down()
-    lift_bot.current_state = 9
+    lift_bot.current_state = lift_bot.STATE_DOWN
 if pins.TOP.value() == 0:
     lift_bot.lift_state = "OPEN"
 if pins.BOTTOM.value() == 0:
@@ -16,3 +16,6 @@ else:
 while True:
     lift_bot.button_handler()
     lift_bot.motor_control()
+
+
+
