3a91609cda0def5c4084aff7135bd72604d5ec80
CassySch <94879749+CassySch@users.noreply.github.com>
2023-12-19 10:35:15 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ lift_bot.connect()
 if not pins.TOP.value() == 0 or not pins.BOTTOM.value() == 0:
     lift_bot.lift_down()
     lift_bot.current_state = lift_bot.STATE_DOWN
-if pins.TOP.value() == 0:
+elif pins.TOP.value() == 0:
     lift_bot.lift_state = "OPEN"
-if pins.BOTTOM.value() == 0:
+elif pins.BOTTOM.value() == 0:
     lift_bot.lift_state = "CLOSED"
 else:
     lift_bot.lift_state = "PARTIAL"
