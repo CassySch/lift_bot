04202b34ab679c200099c2180a141ab68b9e93a4
CassySch <94879749+CassySch@users.noreply.github.com>
2023-12-19 11:04:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ lift_bot.connect()
 
 if not (pins.TOP.value() == 0 and pins.BOTTOM.value() == 0):
     lift_bot.lift_down()
-    lift_bot.current_state = lift_bot.STATE_DOWN
+    lift_bot.current_state = 9
 if pins.TOP.value() == 0:
     lift_bot.lift_state = "OPEN"
 if pins.BOTTOM.value() == 0:
@@ -16,6 +16,3 @@ else:
 while True:
     lift_bot.button_handler()
     lift_bot.motor_control()
-
-
-
